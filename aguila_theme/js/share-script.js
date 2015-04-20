0fda595f6bfc829e02fd51df3b58ab69221fc3be
unknown <gaguilar@gaguilar.studiocom.ad>
2015-04-20 10:10:27 -0500

    share script mod

--- aguila_theme/js/share-script.js
+++ aguila_theme/js/share-script.js
@@ -1,8 +1,8 @@
 (function ($) {
     $(document).ready(function () {
+        console.log("shareMap");
         var currentUrl = location.href;
-        $('.field-label-hidden, .social>li').on('click touch', function(e){
-            var mediaUrl = location.href;
+        $('.field-label-hidden, #social>li').on('click touch', function(e){
             if( $(this).hasClass("field-name-field-facebook")){
                  window.open( 'https://www.facebook.com/sharer/sharer.php?u='+currentUrl,
                 '', 'status=1,width=626,height=436,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no');
@@ -10,16 +10,23 @@
             if($(this).hasClass("field-name-field-twitter")){
                 window.open( 'http://twitter.com/share?url='+currentUrl,
                 '','status=1,width=626,height=436,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no');
-            }
+            }            
+            e.preventDefault();
+        });
+
+        $('.container-map').on('click touch', 'li', function(e){
+            currentUrl = location.href;
+            currentUrl = currentUrl+"%23map"
             if($(this).hasClass("twitter")){
-                window.open( 'http://twitter.com/share?url='+ currentUrl+'#map',
+                window.open( 'http://twitter.com/share?url='+ currentUrl,
                 '','status=1,width=626,height=436,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no');
             }
             if( $(this).hasClass("facebook")){
-                 window.open( 'https://www.facebook.com/sharer/sharer.php?u='+currentUrl+'#map',
+                 window.open( 'https://www.facebook.com/sharer/sharer.php?u='+currentUrl,
                 '', 'status=1,width=626,height=436,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no');
             }
             e.preventDefault();
         });
+
     });
 }(jQuery2));
