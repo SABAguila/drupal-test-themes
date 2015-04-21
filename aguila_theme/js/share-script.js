4c3904ba1b4b235eaad4d599420ba2ac2da5526f
unknown <gaguilar@gaguilar.studiocom.ad>
2015-04-20 19:24:44 -0500

    added styles and js for More info Internal Page

--- aguila_theme/js/share-script.js
+++ aguila_theme/js/share-script.js
@@ -27,6 +27,11 @@
             }
             e.preventDefault();
         });
+        $(".field-name-field-link-seguir-leyendo a").click(function(e){
+            $(" .node-fiestas .content .field-item p").show();
+            $(".node-fiestas .field-name-field-link-seguir-leyendo a").hide();
+            e.preventDefault();
+        });
 
     });
 }(jQuery2));
